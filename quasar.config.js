a81694aff375e890a88956f97cd8ba63f7439dae
Daniel Trstansky <daniel.trstansky@nov.sk>
2025-08-04 12:13:51 +0200

    aaa

--- quasar.config.js
+++ quasar.config.js
@@ -8,11 +8,9 @@
 // Configuration for your app
 // https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js
 
-
 const ESLintPlugin = require('eslint-webpack-plugin')
 
-
-const { configure } = require('quasar/wrappers');
+const { configure } = require('quasar/wrappers')
 
 module.exports = configure(function (ctx) {
   return {
@@ -25,15 +23,10 @@ module.exports = configure(function (ctx) {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
     // https://v2.quasar.dev/quasar-cli-webpack/boot-files
-    boot: [
-      'i18n',
-      'axios',
-    ],
+    boot: ['i18n', 'axios'],
 
     // https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js#Property%3A-css
-    css: [
-      'app.scss'
-    ],
+    css: ['app.scss'],
 
     // https://github.com/quasarframework/quasar/tree/dev/extras
     extras: [
@@ -46,15 +39,15 @@ module.exports = configure(function (ctx) {
       // 'roboto-font-latin-ext', // this or either 'roboto-font', NEVER both!
 
       'roboto-font', // optional, you are not bound to it
-      'material-icons', // optional, you are not bound to it
+      'material-icons' // optional, you are not bound to it
     ],
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js#Property%3A-build
     build: {
-      vueRouterMode: 'hash', // available values: 'hash', 'history'
+      vueRouterMode: 'history', // available values: 'hash', 'history'
 
       // transpile: false,
-      // publicPath: '/',
+      publicPath: '/tv',
 
       // Add dependencies for transpiling with Babel (Array of string/regex)
       // (from node_modules, which are by default not transpiled).
@@ -72,12 +65,10 @@ module.exports = configure(function (ctx) {
 
       // https://v2.quasar.dev/quasar-cli-webpack/handling-webpack
       // "chain" is a webpack-chain object https://github.com/neutrinojs/webpack-chain
-      
-      chainWebpack (chain) {
-        chain.plugin('eslint-webpack-plugin')
-          .use(ESLintPlugin, [{ extensions: [ 'js', 'vue' ] }])
+
+      chainWebpack(chain) {
+        chain.plugin('eslint-webpack-plugin').use(ESLintPlugin, [{ extensions: ['js', 'vue'] }])
       }
-      
     },
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js#Property%3A-devServer
@@ -119,17 +110,14 @@ module.exports = configure(function (ctx) {
       // manualPostHydrationTrigger: true,
 
       prodPort: 3000, // The default port that the production server should use
-                      // (gets superseded if process.env.PORT is specified at runtime)
+      // (gets superseded if process.env.PORT is specified at runtime)
 
       maxAge: 1000 * 60 * 60 * 24 * 30,
-        // Tell browser when a file from the server should expire from cache (in ms)
+      // Tell browser when a file from the server should expire from cache (in ms)
 
-      
-      chainWebpackWebserver (chain) {
-        chain.plugin('eslint-webpack-plugin')
-          .use(ESLintPlugin, [{ extensions: [ 'js' ] }])
+      chainWebpackWebserver(chain) {
+        chain.plugin('eslint-webpack-plugin').use(ESLintPlugin, [{ extensions: ['js'] }])
       },
-      
 
       middlewares: [
         ctx.prod ? 'compression' : '',
@@ -144,16 +132,14 @@ module.exports = configure(function (ctx) {
 
       // for the custom service worker ONLY (/src-pwa/custom-service-worker.[js|ts])
       // if using workbox in InjectManifest mode
-      
-      chainWebpackCustomSW (chain) {
-        chain.plugin('eslint-webpack-plugin')
-          .use(ESLintPlugin, [{ extensions: [ 'js' ] }])
+
+      chainWebpackCustomSW(chain) {
+        chain.plugin('eslint-webpack-plugin').use(ESLintPlugin, [{ extensions: ['js'] }])
       },
-      
 
       manifest: {
-        name: `TB Training`,
-        short_name: `TB Training`,
+        name: `TV`,
+        short_name: `TV`,
         description: `A Quasar Project`,
         display: 'standalone',
         orientation: 'portrait',
@@ -205,13 +191,11 @@ module.exports = configure(function (ctx) {
 
       packager: {
         // https://github.com/electron-userland/electron-packager/blob/master/docs/api.md#options
-
         // OS X / Mac App Store
         // appBundleId: '',
         // appCategoryType: '',
         // osxSign: '',
         // protocol: 'myapp://path',
-
         // Windows only
         // win32metadata: { ... }
       },
@@ -223,19 +207,14 @@ module.exports = configure(function (ctx) {
       },
 
       // "chain" is a webpack-chain object https://github.com/neutrinojs/webpack-chain
-      
-      chainWebpackMain (chain) {
-        chain.plugin('eslint-webpack-plugin')
-          .use(ESLintPlugin, [{ extensions: [ 'js' ] }])
-      },
-      
 
-      
-      chainWebpackPreload (chain) {
-        chain.plugin('eslint-webpack-plugin')
-          .use(ESLintPlugin, [{ extensions: [ 'js' ] }])
+      chainWebpackMain(chain) {
+        chain.plugin('eslint-webpack-plugin').use(ESLintPlugin, [{ extensions: ['js'] }])
       },
-      
+
+      chainWebpackPreload(chain) {
+        chain.plugin('eslint-webpack-plugin').use(ESLintPlugin, [{ extensions: ['js'] }])
+      }
     }
   }
-});
+})
--- src/stores/LongPollingStore.js
+++ src/stores/LongPollingStore.js
@@ -1,52 +1,100 @@
 import { defineStore } from 'pinia'
 import dayjs from 'dayjs'
 import { api } from '../boot/axios.js'
+import axios from 'axios'
 
-const id = new URL(location.href).searchParams.get('id') || window.location.pathname.split('/').pop()
+const id = new URL(location.href).searchParams.get('id') || window.location.pathname.split('/').pop() || 'demo'
 
 api.defaults.timeout = 30000
+
+// Interceptory na api instanciu
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('accessToken')
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  (error) => Promise.reject(error)
+)
+
 api.interceptors.response.use(
   (response) => response,
-  (error) => {
-    console.log(error)
+  async (error) => {
+    if (error.response?.status === 401) {
+      console.warn('401 Unauthorized – token zmazaný, získavam nový a reloadujem...')
+
+      localStorage.removeItem('accessToken')
+      delete api.defaults.headers.common.Authorization
+
+      const store = useLongPollingStore()
+      await store.fetchAccessToken()
+
+      location.reload()
+    }
+
     return Promise.reject(error)
   }
 )
 
+// NOVÝ connectApi - čistá axios inštancia pre connect
+const connectApi = axios.create({
+  baseURL: '/', // alebo rovnaký base ak potrebuješ
+  timeout: 30000,
+  headers: { common: { 'X-Requested-With': 'sk.nov.weblock' } }
+})
+
 export const useLongPollingStore = defineStore('longPolling', {
+  state: () => ({
+    events: []
+  }),
+
   actions: {
-    start() {
-      this.runCommand(this.dispatch)
+    async start() {
+      await this.ensureAccessToken()
+      this.runCommand()
     },
 
-    async runCommand(dispatch) {
+    async ensureAccessToken() {
+      const token = localStorage.getItem('accessToken')
+      if (token) {
+        api.defaults.headers.common.Authorization = `Bearer ${token}`
+        return
+      }
+      await this.fetchAccessToken()
+    },
+
+    async fetchAccessToken() {
+      try {
+        const res = await connectApi.get(`/connect?id=${id}`)
+        const token = res?.data?.accessToken
+        if (!token) throw new Error('Token is undefined')
+
+        localStorage.setItem('accessToken', token)
+        api.defaults.headers.common.Authorization = `Bearer ${token}`
+        console.log('Token získaný, pokračujem...')
+      } catch (err) {
+        console.warn('Získavanie tokenu zlyhalo. Skúšam znova o 10s...')
+        await new Promise((resolve) => setTimeout(resolve, 10000))
+        await this.fetchAccessToken()
+      }
+    },
+
+    async runCommand() {
       const op = { timeout: 30000 }
       try {
-        const res = await api.get(id + '?timestamp=' + dayjs().unix(), op)
-        console.log(res.data[0])
-        this.events = res.data[0].events.map((event) => {
-          const dateRegex = /\d{1,2}\.\d{1,2}\.\d{4}/
-          const dateMatch = event.start.match(dateRegex)
-          const startDate = dateMatch ? dateMatch[0] : ''
-          const startTime = event.start.replace(startDate, '').trim()
-
-          return {
-            ...event,
-            start: startTime
-          }
-        })
-        dispatch('execCommand', res.data)
+        const res = await api.get(`${id}?timestamp=${dayjs().unix()}`, op)
+        this.events = res.data.events.map((event) => ({ ...event }))
       } catch (error) {
         if (error.code === 'ECONNABORTED') {
           console.log('runCommand timeout')
         }
       } finally {
-        setTimeout(() => this.runCommand(dispatch), 30000)
+        // Delay medzi requestami (napríklad 2 sekundy)
+        await new Promise((resolve) => setTimeout(resolve, 2000))
+        this.runCommand()
       }
-    },
-
-    execCommand({ commit }, data) {
-      commit('updateEvents', data)
     }
   }
 })
